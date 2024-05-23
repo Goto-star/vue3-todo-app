a391779334d8f4149b969d0945e23ec6adaeecbe
Goto-star <goyu620@gmail.com>
2024-05-23 14:50:31 +0900

    .gitignore .prettierrcファイルの作成

--- todo.js
+++ todo.js
@@ -1,53 +1,53 @@
-const { createApp } = Vue;
+const { createApp } = Vue
 
-(generateUUID = () => {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+;(generateUUID = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     let r = (Math.random() * 16) | 0,
-      v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+      v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
 }),
   createApp({
     data() {
       return {
-        newTodo: "",
-        editTodo: "",
+        newTodo: '',
+        editTodo: '',
         editIndex: -1,
-        todos: JSON.parse(localStorage.getItem("todos")) || [],
-      };
+        todos: JSON.parse(localStorage.getItem('todos')) || [],
+      }
     },
     methods: {
       addTodo() {
-        if (this.newTodo.trim() === "") return;
+        if (this.newTodo.trim() === '') return
         this.todos.unshift({
           id: generateUUID(),
           text: this.newTodo,
-        });
-        this.newTodo = "";
-        this.saveTodo();
+        })
+        this.newTodo = ''
+        this.saveTodo()
       },
       editTask(index) {
-        this.editIndex = index;
-        this.editTodo = this.todos[index].text;
+        this.editIndex = index
+        this.editTodo = this.todos[index].text
       },
       saveEdit(index) {
         if (this.editTodo.trim()) {
-          this.todos[index].text = this.editTodo;
-          this.editIndex = -1;
-          this.editTodo = "";
-          this.saveTodo();
+          this.todos[index].text = this.editTodo
+          this.editIndex = -1
+          this.editTodo = ''
+          this.saveTodo()
         }
       },
       cancelEdit() {
-        this.editIndex = -1;
-        this.editTodo = "";
+        this.editIndex = -1
+        this.editTodo = ''
       },
       removeTodo(index) {
-        this.todos.splice(index, 1);
-        this.saveTodo();
+        this.todos.splice(index, 1)
+        this.saveTodo()
       },
       saveTodo() {
-        localStorage.setItem("todos", JSON.stringify(this.todos));
+        localStorage.setItem('todos', JSON.stringify(this.todos))
       },
     },
-  }).mount("#app");
+  }).mount('#app')
