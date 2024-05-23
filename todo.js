734b475b5b8ea11eb91b7e9115e04526b1cd6c14
Goto-star <goyu620@gmail.com>
2024-05-23 11:13:54 +0900

    TODOアプリの登録・一覧表示機能の実装

---
+++ todo.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      newTodo: "",
+      todos: JSON.parse(localStorage.getItem("todos")) || [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() === "") return;
+      this.todos.unshift({ id: this.todos.length + 1, text: this.newTodo });
+      this.newTodo = "";
+      this.saveTodo();
+    },
+    saveTodo() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+}).mount("#app");
