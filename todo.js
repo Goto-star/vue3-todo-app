c95d6a70bf3cd0246d7d125faa3f0ece7c34351b
Goto-star <goyu620@gmail.com>
2024-05-23 14:00:19 +0900

    TODOアプリの編集機能の実装

--- todo.js
+++ todo.js
@@ -1,25 +1,53 @@
 const { createApp } = Vue;
 
-createApp({
-  data() {
-    return {
-      newTodo: "",
-      todos: JSON.parse(localStorage.getItem("todos")) || [],
-    };
-  },
-  methods: {
-    addTodo() {
-      if (this.newTodo.trim() === "") return;
-      this.todos.unshift({ id: this.todos.length + 1, text: this.newTodo });
-      this.newTodo = "";
-      this.saveTodo();
+(generateUUID = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    let r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}),
+  createApp({
+    data() {
+      return {
+        newTodo: "",
+        editTodo: "",
+        editIndex: -1,
+        todos: JSON.parse(localStorage.getItem("todos")) || [],
+      };
     },
-    removeTodo(index) {
-      this.todos.splice(index, 1);
-      this.saveTodo();
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim() === "") return;
+        this.todos.unshift({
+          id: generateUUID(),
+          text: this.newTodo,
+        });
+        this.newTodo = "";
+        this.saveTodo();
+      },
+      editTask(index) {
+        this.editIndex = index;
+        this.editTodo = this.todos[index].text;
+      },
+      saveEdit(index) {
+        if (this.editTodo.trim()) {
+          this.todos[index].text = this.editTodo;
+          this.editIndex = -1;
+          this.editTodo = "";
+          this.saveTodo();
+        }
+      },
+      cancelEdit() {
+        this.editIndex = -1;
+        this.editTodo = "";
+      },
+      removeTodo(index) {
+        this.todos.splice(index, 1);
+        this.saveTodo();
+      },
+      saveTodo() {
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+      },
     },
-    saveTodo() {
-      localStorage.setItem("todos", JSON.stringify(this.todos));
-    },
-  },
-}).mount("#app");
+  }).mount("#app");
