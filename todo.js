5219bb94ff66059e0f10aaf149d5dff8f3313391
Goto-star <goyu620@gmail.com>
2024-05-29 14:01:04 +0900

    ソースコードのリファクタリング && gitignoreファイルの修正

--- todo.js
+++ todo.js
@@ -1,46 +1,67 @@
-const { createApp } = Vue
+const { createApp, ref } = Vue
 
 createApp({
-  data() {
-    return {
-      newTodo: '',
-      editTodo: '',
-      editIndex: -1,
-      todos: JSON.parse(localStorage.getItem('todos')) || [],
+  setup() {
+    const fetchSavedTodos = () => {
+      const savedTodos = localStorage.getItem('todos')
+      return savedTodos ? JSON.parse(savedTodos) : []
     }
-  },
-  methods: {
-    addTodo() {
-      if (this.newTodo.trim() === '') return
-      this.todos.unshift({
+
+    const newTodo = ref('')
+    const editTodo = ref('')
+    const editIndex = ref(-1)
+    const todos = ref(fetchSavedTodos())
+
+    const addTodo = () => {
+      if (newTodo.value.trim() === '') return
+      todos.value.push({
         id: uuid.v4(),
-        text: this.newTodo,
+        text: newTodo.value,
+        done: false,
       })
-      this.newTodo = ''
-      this.saveTodo()
-    },
-    editTask(index) {
-      this.editIndex = index
-      this.editTodo = this.todos[index].text
-    },
-    saveEdit(index) {
-      if (this.editTodo.trim()) {
-        this.todos[index].text = this.editTodo
-        this.editIndex = -1
-        this.editTodo = ''
-        this.saveTodo()
+      console.log('After adding todo:', JSON.stringify(todos.value))
+      newTodo.value = ''
+      saveTodo()
+    }
+
+    const editTask = (index) => {
+      editIndex.value = index
+      editTodo.value = todos.value[index].text
+    }
+
+    const saveEdit = (index) => {
+      if (editTodo.value.trim()) {
+        todos.value[index].text = editTodo.value
+        editIndex.value = -1
+        editTodo.value = ''
+        saveTodo()
       }
-    },
-    cancelEdit() {
-      this.editIndex = -1
-      this.editTodo = ''
-    },
-    removeTodo(index) {
-      this.todos.splice(index, 1)
-      this.saveTodo()
-    },
-    saveTodo() {
-      localStorage.setItem('todos', JSON.stringify(this.todos))
-    },
+    }
+
+    const cancelEdit = () => {
+      editIndex.value = -1
+      editTodo.value = ''
+    }
+
+    const removeTodo = (index) => {
+      todos.value.splice(index, 1)
+      saveTodo()
+    }
+
+    const saveTodo = () => {
+      localStorage.setItem('todos', JSON.stringify(todos.value))
+    }
+
+    return {
+      newTodo,
+      editTodo,
+      editIndex,
+      todos,
+      addTodo,
+      editTask,
+      saveEdit,
+      cancelEdit,
+      removeTodo,
+    }
   },
 }).mount('#app')
